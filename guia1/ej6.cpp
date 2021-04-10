fb2c91ce5a269a4d1e00cc28f53bc659a2e9057c
cmamani02 <33373307+cmamani02@users.noreply.github.com>
2021-04-09 22:12:43 -0300

    acomodo un poco

--- guia1/ej6.cpp
+++ guia1/ej6.cpp
@@ -31,15 +31,42 @@ tuple<int,int> sume(tuple<int,int> a, tuple<int,int> b){
 // b: implementacion recursiva
 // i es es indice de el elemento a procesar
 // j es el monto a alcanzar
+
+//cc(0, k) es la solucion
 tuple<int,int> cc(vector<int> &B, int i, int j){
     if(i == B.size()) return make_tuple (j > 0 ? INF : 0, 0);
+    
+    if( c < 0 ) make_tuple (0, 0);
     
     tuple<int, int> aux = make_tuple (B[i], 1);
     tuple<int,int> r = minime( sume(aux, cc(B, i+1, j-B[i])), 
                                 cc(B, i+1, j) );
     
+    
+    
     return r;
 }
+// Hola, para el ejercicio 6.a se me ocurrio esta funcion recursiva para cc(B,c):
+
+// || 
+//(cprima, q)
+//k valor objetivo
+// cc({b1 ..bn}, c) = 
+
+// { ( c > 0 ? INFINITO : 0 , 0 ) si n = 0
+
+// { min{ (bn, 1) + cc({b1 ..bn-1}, c-bn) ,   si n > 0
+// 			c({b1 ..bn-1}, c) } 
+
+// ||
+
+// || en el caso base no tengo billetes disponibles:
+// si c > 0: INFINITO parece un valor apropiado.
+// si c <= 0: ya no hay nada que pagar entonces devuelvo 0.
+// y en ambos casos q = 0 porque no hay billetes. ||
+
+
+
 
 
 int main(){
@@ -54,19 +81,4 @@ int main(){
 
 }
 
-// Hola, para el ejercicio 6.a se me ocurrio esta funcion recursiva para cc(B,c):
-
-// || 
-// cc({b1 ..bn}, c) = 
-// { ( c > 0 ? INFINITO : 0 , 0 ) si n = 0
-
-// { min{ (bn, 1) + cc({b1 ..bn-1}, c-bn) ,   si n > 0
-// 			c({b1 ..bn-1}, c) } 
-
-// ||
-
-// || en el caso base no tengo billetes disponibles:
-// si c > 0: INFINITO parece un valor apropiado.
-// si c <= 0: ya no hay nada que pagar entonces devuelvo 0.
-// y en ambos casos q = 0 porque no hay billetes. ||
 
